38309d92c549042dffd79d914860bcea4eed8b2c
gustavo <gustavoaguirregomez10@gmail.com>
2024-11-17 22:14:16 -0300

    UPDATE CORS2

--- server/src/app.js
+++ server/src/app.js
@@ -9,18 +9,30 @@ dotenv.config()
 
 const app = express();
 app.use(morgan("dev"));
+
 app.use(cors({
   origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:5173', 'https://crud-mern-three-omega.vercel.app'];
+    const allowedOrigins = [
+      'http://localhost:5173',
+      'https://crud-mern-three-omega.vercel.app',
+      'https://crud-mern-32xrglv8y-gustavos-projects-131b8237.vercel.app' // Incluye cualquier URL adicional que uses
+    ];
+
+    // Permite peticiones sin origen (p. ej., postman) y dominios permitidos
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
+  credentials: true, // Permite envío de cookies
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'], // Permite ciertos encabezados
 }));
+app.options('*', cors()); // Maneja solicitudes OPTIONS para cualquier ruta
+
 app.use(express.json());
-express.urlencoded()
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api', taskRoutes)
